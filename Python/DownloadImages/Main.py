878ca5aec8c85a8e3ac3200df23993e34f8f2591
pchase95 <peter.chase@ingagetech.com>
2016-05-14 08:55:41 -0400

    update

--- Python/DownloadImages/Main.py
+++
@@ -1,7 +0,0 @@
-import urllib.request
-
-def getImage(url):
-    name = 'pepe' + url[-3:]
-    urllib.request.urlretrieve(url, name)
-
-getImage('https://40.media.tumblr.com/8661041b26d1f4b05883a24e48ed35cd/tumblr_inline_o67ojpRKX31u6d6kp_540.jpg')
